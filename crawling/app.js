3a579c11dd8504a55df2b1a36f13a8b256e797a2
cjh724 <jh.cho133104@gmail.com>
2018-09-29 22:30:27 +0900

    install express and iconv module, movie crawling complete ref blog

---
+++ crawling/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const request = require('request');
+const cheerio = require('cheerio');
+const Iconv = require('iconv').Iconv;
+const iconv = new Iconv('CP949', 'utf-8//translit//ignore');
+
+app.get('/crawlingMoive', function(req, res, next) {
+  let url = "https://movie.naver.com/movie/sdb/rank/rmovie.nhn";
+
+  request({url, encoding: null}, function(error, response, body){
+    let htmlDoc = iconv.convert(body).toString();
+    let resultArr = [];
+
+    const $ = cheerio.load(htmlDoc);
+    let colArr = $(".tit3");
+    for(let i = 0; i < colArr.length; i++) {
+        resultArr.push(colArr[i].children[1].attribs.title);
+    }
+
+    res.json(resultArr);
+  });
+});
+module.exports = app;
+
+app.listen(3000, function() {
+  console.log('Connected, 3000 port!');
+});
